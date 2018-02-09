a64f9df4b0b55a508ac83170dcc65161e8cb0795
nick197504 <79252568@qq.com>
2018-02-09 09:31:37 +0800

    first commit

---
+++ data/couponDetail.js
@@ -0,0 +1,28 @@
+var localData = { 
+  "data":{ 
+    "skuId":"24821374715",
+     "skuName":"\u673a\u4f34 vivox9\u624b\u673a\u58f3\u4fdd\u62a4\u5957\u7537\u5973\u6b3e\u78e8\u7802\u9632\u6454vivo x9splus \u9002\u7528\u4e8ex9\/x9s \u51b0\u96ea\u806a\u660e-\u9ed1\u5170 vivox9",
+    "skuDesc":  "\u7ed9\u4f60\u7684\u7231\u673a\u6765\u4e2a\u5957\u5427\uff01\u5168\u5305\u8f6f\u58f3\uff0c\u591a\u8272\u53ef\u9009\uff0c\u4f18\u96c5\u5927\u6c14\uff0c\u77ac\u95f4\u79d2\u4e0a\u6863\u6b21\uff01\u4ec5\u97009\u5757\u94b1\uff01\uff01\uff01\n\u3010\u65b0\u54c1\u8840\u4e8f\u4fc3\u9500\u3011vivo\u78e8\u7802\u9632\u6454\u624b\u673a\u58f3 \u9002\u7528\u4e8ex9\/x9s \n-------------------- \n\u4eac\u4e1c\u4ef7\uff1a39",
+   "materiaUrl":"http:\/\/item.jd.com\/24821374715.html",
+    "picUrl":
+   "http:\/\/img14.360buyimg.com\/n1\/jfs\/t17233\/318\/155784406\/166849\/9652a57c\/5a607006N0bd1f13f.jpg",
+    "wlPrice":"39.00",
+    "wlPrice_after":"9.00",
+    "wlCommissionShare":"30",
+    "couponList":"http:\/\/coupon.m.jd.com\/coupons\/show.action?key=ae29b7b80071490cb9c4c1184370eee8&roleId=10257836&to=item.jd.com\/24821374708.html",
+    "bindType":null,
+    "discount":"30.00",
+    "quota":null,
+    "platformType":null,
+    "endTime":2147483647,
+    "cnameicon":"",
+    "cname":"\u6570\u7801\u529e\u516c",
+    "goodstype":0
+  },
+  "message":"success",
+   "status_code":200
+}
+
+module.exports = {
+  couponInfo: localData
+}
